a549d59768f579dfd1ea04cd02de4362e5ee769d
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-08-08 00:28:26 -0600

    ADD
    Delete post method

--- api.js
+++ api.js
@@ -115,3 +115,30 @@ const updatePost = async (event) => {
 
     return response;
 };
+
+const deletePost = async (event) => {
+    const response = { statusCode: 200 };
+
+    try {
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Key: marshall({ postId: event.pathParameters.postId }),
+        };
+        const deleteResult = await db.send(new DeleteItemCommand(params));
+
+        response.body = JSON.stringify({
+            message: "Successfully deleted post.",
+            deleteResult,
+        });
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to delete post.",
+            errorMsg: e.message,
+            errorStack: e.stack,
+        });
+    }
+
+    return response;
+};
