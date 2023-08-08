58ba279f65cd88ab6e3890255d44554c72857c36
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-08-08 00:39:41 -0600

    ADD
    Get all post method

--- api.js
+++ api.js
@@ -142,3 +142,27 @@ const deletePost = async (event) => {
 
     return response;
 };
+
+const getAllPosts = async () => {
+    const response = { statusCode: 200 };
+
+    try {
+        const { Items } = await db.send(new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME }));
+
+        response.body = JSON.stringify({
+            message: "Successfully retrieved all posts.",
+            data: Items.map((item) => unmarshall(item)),
+            Items,
+        });
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to retrieve posts.",
+            errorMsg: e.message,
+            errorStack: e.stack,
+        });
+    }
+
+    return response;
+};
