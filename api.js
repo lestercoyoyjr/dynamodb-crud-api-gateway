cecc206f22b8eb765fb92805eed744428ad95c86
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-08-07 23:37:39 -0600

    ADD
    CreatePost function

--- api.js
+++ api.js
@@ -37,3 +37,31 @@ const getPost = async (event) => {
 
     return response;
 };
+
+const createPost = async (event) => {
+    const response = { statusCode: 200 };
+
+    try {
+        const body = JSON.parse(event.body);
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Item: marshall(body || {}),
+        };
+        const createResult = await db.send(new PutItemCommand(params));
+
+        response.body = JSON.stringify({
+            message: "Successfully created post.",
+            createResult,
+        });
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to create post.",
+            errorMsg: e.message,
+            errorStack: e.stack,
+        });
+    }
+
+    return response;
+};
